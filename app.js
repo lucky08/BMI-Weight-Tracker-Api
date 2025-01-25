09abe51ec659ff50ba531289d815ed733135aef7
Jingting Chen <jingtingchen@Jingtings-MBP.zyxel.com>
2025-01-24 21:35:23 -0600

    Initialize node express app

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const process = require("process");
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: false, limit: "5mb" }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("withCredentials", "*");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log(`${req.method}\t${req.url} at ${new Date().toISOString()}`);
+  if (req.method == "POST" || req.method == "PATCH" || req.method == "PUT")
+    console.log(req.body);
+  next();
+});
+
+if (process.env.PORT) {
+  app.listen(process.env.PORT);
+  console.log("Express started at http://localhost:" + process.env.PORT);
+}
+
+module.exports = app;
---
+++ knexfile.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+// Update with your config settings.
+module.exports = {
+  development: {
+    client: "mysql",
+    connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_DB,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
