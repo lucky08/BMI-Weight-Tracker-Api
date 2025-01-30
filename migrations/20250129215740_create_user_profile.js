46bdf2523aa5e244a44d5bb762b53f224d0deff4
Jingting Chen <jingtingchen@Jingtings-MBP.zyxel.com>
2025-01-30 10:58:39 -0600

    Add user_profile table migration

---
+++ migrations/20250129215740_create_user_profile.js
@@ -0,0 +1,34 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .table("device", function (table) {
+      table.index("uuid", "device_uuid_index");
+    })
+    .createTable("user_profile", function (table) {
+      table.bigincrements("id").primary();
+      table.string("user_name", 255).notNullable();
+      table.bigInteger("age");
+      table.string("gender", 200);
+      table.bigInteger("height");
+      table
+        .string("uuid", 255)
+        .notNullable()
+        .references("uuid")
+        .inTable("device");
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("user_profile")
+    .table("device", function (table) {
+      table.dropIndex("uuid", "device_uuid_index"); // Remove index if rollback
+    });
+};
