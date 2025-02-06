5c1a0df346db0ddb64476e8a918a2c7e1942ddd6
Jingting Chen <jingtingchen@Jingtings-MBP.zyxel.com>
2025-02-05 22:05:57 -0600

    Update date_time type in weight_date column

---
+++ migrations/20250206035309_modify_weight_date_date_time.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("weight_date", function (table) {
+    table.string("date_time").alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("weight_date", function (table) {
+    table.timestamp("date_time").alter();
+  });
+};
